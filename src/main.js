6cf211825df51ba11284b47b475372ba0f32346e
Justin Fong <justinvf2018@gmail.com>
2025-01-29 18:49:20 -0800

    filling in the main.js comment line

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 /**
  * Name: Justin Fong
  * 
- * Title:
+ * //I hope you get this reference, I couldn't think of anything else
+ * Title: Rocket Patrollin': Tryna Catch Me 
  * 
- * Time of Completion: start @ 3:05PM
+ * Time of Completion: ~7hrs
  * 
  * Mods: 
  *  - Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (5) - 2hrs
@@ -11,12 +12,12 @@
  *  - Display the time remaining (in seconds) on the screen (3) - 2hrs
  *  - Create a new title screen (e.g., new artwork, typography, layout) (3) - 0.5hr
  *  - Create 4 new explosion sound effects and randomize which one plays on impact (3) - 0.5hr
- *      - from: https://sfbgames.itch.io/chiptone
- *  - Add your own (copyright-free) looping background music to the Play scene (keep the volume low and be sure that multiple instances of your music don't play when the game restarts) (1)
- *      - https://www.youtube.com/watch?v=HeVIaLm0tyw&list=OLAK5uy_np776jRtjA4yqUGsgR4xIS2BK6IGIIuS0&index=2
- *      - https://artlist.io/royalty-free-music/song/night-rider/4780
+ *  - Add your own (copyright-free) looping background music to the Play scene (keep the volume low and be sure that multiple instances of your music don't play when the game restarts) (1) 
  * 
  * Citations:
+ *      - https://www.youtube.com/watch?v=HeVIaLm0tyw&list=OLAK5uy_np776jRtjA4yqUGsgR4xIS2BK6IGIIuS0&index=2
+ *      - https://artlist.io/royalty-free-music/song/night-rider/4780
+ *      - https://sfbgames.itch.io/chiptone
  */
 
 console.log("Hello World")
